88439c52946eb0fcdb8710002b6d44d2b67d9a9f
lwalton101 <waltonfamily01@outlook.com>
2026-07-17 19:35:24 +0100

    add ui project

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -2,9 +2,11 @@ rootProject.name = "connect-4"
 
 include("core")
 include("cli")
+include("ui")
 
 project(":core").name = "connect4-core"
 project(":cli").name = "connect4-cli"
+project(":ui").name = "connect4-ui"
 
 pluginManagement {
     repositories {
